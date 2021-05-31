6bf9c09d0e5333a1c9c4be0b49fdc560d6812fb8
cpratik711 <cpratik1803@gmail.com>
2021-05-31 13:24:58 +0530

    Added screenshots and videos

--- app/src/main/java/com/pratik/voiceinput/VoiceRecognitionActivity.kt
+++ app/src/main/java/com/pratik/voiceinput/VoiceRecognitionActivity.kt
@@ -1,16 +1,12 @@
-package com.pratik.uicomponentcreation
+package com.pratik.voiceinput
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
-import android.text.TextUtils
-import android.util.Log
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
-import com.pratik.uicomponentcreation.databinding.ActivityVoiceRecognitionBinding
-import java.util.*
+import com.pratik.voiceinput.databinding.ActivityVoiceRecognitionBinding
 
 
 class VoiceRecognitionActivity : AppCompatActivity() {
@@ -38,12 +34,14 @@ class RecognitionContract : ActivityResultContract<Int, String>() {
     override fun createIntent(context: Context, input: Int?): Intent {
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US")
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to Text")
         return intent
     }
 
 
     override fun parseResult(resultCode: Int, intent: Intent?): String {
-        val spokenText = intent?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0) ?: ""
+        val spokenText =
+            intent?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0) ?: ""
         return spokenText
     }
 }
